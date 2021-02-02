4305d1eef38d4b495c1937a0d8d85ecc54e36750
Eder Oliveira Lima <64004309+Nxrth-x@users.noreply.github.com>
2021-02-02 08:24:27 -0300

    Added docstrings and changed websocket link
    Added docstrings to the socket.onmessage function and comments

--- src/static/main.js
+++ src/static/main.js
@@ -1,21 +1,33 @@
-const socket = new WebSocket("ws://d6eff41e2a4b.ngrok.io/");
+// @ts-check
 
-// Função executada quando receber uma mensagem
+const socket = new WebSocket("ws://localhost:8080/");
+
+/**
+ * Renders the all the messages in the UI when a message is recieved
+ * 
+ * @param {{data:string; [key: string]: any}} message 
+ * @returns {void};
+ */
 socket.onmessage = (message) => {
   const { data } = message;
   const messages = JSON.parse(data);
+
+  // Renders the messages in the UI
   const ul = document.querySelector("ul");
   ul.innerHTML = "";
-  messages.map((item) => {
+  messages.forEach((item) => {
     const liElement = document.createElement("li");
     liElement.innerText = item;
     ul.appendChild(liElement);
   });
 };
 
-// Função executada quando  o formulário for submetido
+// Executed when the form is submitted
 document.querySelector("form").addEventListener("submit", (event) => {
+  // Prevents the form from submitting
   event.preventDefault();
+
+  // Gets the value from the input element and deletes it after sending the message
   const inputElement = document.querySelector("input");
   const { value: word } = inputElement;
   socket.send(word);
